1e6f68e644fbd2a116385040afbbc1808ac35bec
Sabar Dasgupta <sabar@orchive.com>
2014-11-07 23:31:25 -0800

    toyota api integration and figuring outing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
+require "uri"
+require "net/http"
+
 class EventsController < ApplicationController
-  require 'yaml'
 
   def trigger
     @type = params[:type]
@@ -11,14 +13,38 @@ class EventsController < ApplicationController
       num = YAML.load_file('config/secrets.yml')['twilio']['number']
       twilio_client = Twilio::REST::Client.new sid, secret
       twilio_client.account.sms.messages.create(
-        :from => "#{num}",
-        :to => "+17329304455",
-        :body => "An accident has occurred!"
+        from: "#{num}",
+        to: "+17329304455",
+        body: "An accident has occurred!"
       )
 
       Notifier.trigger_response("sabar.dasgupta@gmail.com").deliver
     end
 
     render nothing: true
+  end
+
+  def test_post
+    secret = YAML.load_file('config/secrets.yml')['toyota']['secret']
+
+    params =  {
+      developerkey: secret,
+      responseformat: 'json',
+      userid: 'ITCUS_USERID_092'
+    }
+    x = Net::HTTP.post_form(URI.parse('https://api-jp-t-itc.com/GetUserInfo'), params)
+    @user_info = x.body
+
+    params = {
+      developerkey: secret,
+      responseformat: 'json',
+      userid: 'ITCJP_USERID_038',
+      infoids: '[Spd]'
+    }
+
+    x = Net::HTTP.post_form(URI.parse('https://api-jp-t-itc.com/GetVehicleInfo'), params)
+    @vehicle_info = x.body
+
+    render json: [@user_info, @vehicle_info]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'statics#home'
   get '/trigger_event/:type', to: 'events#trigger'
+  get '/test', to: 'events#test_post'
 end
